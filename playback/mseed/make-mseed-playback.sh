d096d3e9b5a608717059c0c1ff8d93d40fdadc4c
Joachim Saul <saul@gfz-potsdam.de>
2013-01-15 12:19:25 +0100

    add hardcoded default config

--- playback/mseed/make-mseed-playback.sh
+++ playback/mseed/make-mseed-playback.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # specify event id(s) on the command line
-
 host="geofon-proc"
 #user="mkplayback" # max 10 characters allowed by spread
+arch="geofon-acqui:18001"
+
 
 for evid in "$@"
 do
-    python make-mseed-playback.py -H "$host" -u "$user" -E "$evid" --debug 2>&1 |
+    python make-mseed-playback.py -H "$host" -I arclink://$arch --user "$user" -E "$evid" --debug 2>&1 |
+#   python make-mseed-playback.py -H "$host" -I arclink://$arch --user "$user" -E "$evid" --debug 2>&1 |
     tee $evid.log
 done
 
