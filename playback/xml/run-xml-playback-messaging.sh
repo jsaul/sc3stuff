d658c7908e1209ec584bc55000587ba73f2f5e3f
Joachim Saul <saul@gfz-potsdam.de>
2013-01-21 17:22:12 +0100

    added script

---
+++ playback/xml/run-xml-playback-messaging.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+###### configuration #######
+# Give name of target host
+target_host=st127.gfz-potsdam.de
+############################
+
+
+for evt in "$@"
+do
+    python xml-playback-messaging.py \
+      --xml "$evt"/objects.xml \
+      --speed 1 \
+      --host $target_host \
+      --user "" \
+      --debug
+done
