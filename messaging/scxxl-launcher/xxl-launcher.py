70a2a06a5aff2d5b9dcc29f1217b3fd7d3580926
Joachim Saul <saul@gfz-potsdam.de>
2014-09-29 11:36:32 +0200

    testing...

--- messaging/scxxl-launcher/xxl-launcher.py
+++ messaging/scxxl-launcher/xxl-launcher.py
@@ -5,14 +5,18 @@ from eventclient import EventClient, info,error,debug
 class Launcher(EventClient):
 
     def changed_origin(self, event_id, previous_id, current_id):
-        EventClient.changed_origin(self, event_id, previous_id, current_id)
         info("Launcher.changed_origin") 
+        info("event %s: CHANGED preferredOriginID" % event_id)
+        info("    from %s" % previous_id)
+        info("      to %s" % current_id)
         self.cleanup()
 
 
     def changed_magnitude(self, event_id, previous_id, current_id):
-        EventClient.changed_origin(self, event_id, previous_id, current_id)
         info("Launcher.changed_magnitude") 
+        info("event %s: CHANGED preferredMagnitudeID" % event_id)
+        info("    from %s" % previous_id)
+        info("      to %s" % current_id)
         self.cleanup()
     
 
