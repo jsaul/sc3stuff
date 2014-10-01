efa4fe432fa20f0a875bf2459bb2bb61eff89a22
Joachim Saul <saul@gfz-potsdam.de>
2014-10-01 13:08:14 +0200

    Now actually starts something...

--- messaging/scxxlmag-launch/scxxlmag-launch.py
+++ messaging/scxxlmag-launch/scxxlmag-launch.py
@@ -1,8 +1,13 @@
-import sys, traceback
+import sys, os, traceback
 from eventclient import EventClient, info,error,debug
 
 
 class Launcher(EventClient):
+
+    def __init__(self):
+        EventClient.__init__(self)
+        self._program = './program.sh %s'
+        self._launched = []
 
     def changed_origin(self, event_id, previous_id, current_id):
         info("Launcher.changed_origin") 
@@ -10,6 +15,7 @@ class Launcher(EventClient):
         info("    from %s" % previous_id)
         info("      to %s" % current_id)
         self.cleanup()
+        self.launch(event_id)
 
 
     def changed_magnitude(self, event_id, previous_id, current_id):
@@ -18,7 +24,16 @@ class Launcher(EventClient):
         info("    from %s" % previous_id)
         info("      to %s" % current_id)
         self.cleanup()
-    
+
+
+    def launch(self, event_id):
+        if event_id in self._launched:
+            return # nothing to do any more
+        cmd = self._program % event_id
+        cmd += " &"
+        info("starting '%s'" % cmd)
+        os.system(cmd)
+        self._launched.append(event_id) 
 
 app = Launcher()
 sys.exit(app())
