f6c27c83818b0596937f5aff430e0fd9ab38c2bd
Joachim Saul <saul@gfz-potsdam.de>
2014-10-01 22:11:50 +0200

    minor update

--- messaging/scxxlmag-launch/scxxlmag-launch.py
+++ messaging/scxxlmag-launch/scxxlmag-launch.py
@@ -6,7 +6,9 @@ class Launcher(EventClient):
 
     def __init__(self):
         EventClient.__init__(self)
-        self._program = './program.sh %s'
+        self._program = './scxxl-wrap.sh %s'
+        self._minmag = 5.0
+        self._maxdep = 150.
         self._launched = []
 
     def changed_origin(self, event_id, previous_id, current_id):
@@ -15,7 +17,7 @@ class Launcher(EventClient):
         info("    from %s" % previous_id)
         info("      to %s" % current_id)
         self.cleanup()
-        self.launch(event_id)
+#       self.launch(event_id)
 
 
     def changed_magnitude(self, event_id, previous_id, current_id):
@@ -24,11 +26,23 @@ class Launcher(EventClient):
         info("    from %s" % previous_id)
         info("      to %s" % current_id)
         self.cleanup()
+        self.launch(event_id)
 
 
     def launch(self, event_id):
         if event_id in self._launched:
             return # nothing to do any more
+        org = self._origin[self._preferredOriginID[event_id]]
+        dep = org.depth().value()
+        mag = self._magnitude[self._preferredMagnitudeID[event_id]]
+        mag = mag.magnitude().value()
+        if dep > self._maxdep:
+            info("Launcher.launch event %s too deep" % event_id)
+            return
+        if mag < self._minmag:
+            info("Launcher.launch event %s too small" % event_id)
+            return
+
         cmd = self._program % event_id
         cmd += " &"
         info("starting '%s'" % cmd)
