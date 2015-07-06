e55cb4c46d190a58952c88dc48d97782025da244
Joachim Saul <saul@gfz-potsdam.de>
2015-07-06 17:38:42 +0200

    Added README

---
+++ set-magnitude-type/README.md
@@ -0,0 +1,6 @@
+setmagtype
+==========
+
+* sets the preferred magnitude type for an event
+* sends the required notifiers
+* includes support for moment-tensor Mw
--- set-magnitude-type/test.sh
+++ set-magnitude-type/test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#python set-magnitude-type.py --xml $HOME/Work/Picker/data/2015/gfz2015iatp/gfz2015iatp.xml
-
 evid=gfz2015ncme
 #evid=gfz2015iatp
 python setmagtype.py -H geofon-proc -d mysql://sysop:sysop@geofon-proc/seiscomp3 -E $evid --magnitude-type Mw
