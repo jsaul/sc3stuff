c31f6c50170ea56fa1a2ffa85b72d7e20c3cd05d
Joachim Saul <saul@gfz-potsdam.de>
2015-09-22 14:30:17 +0200

    cleanup

--- waveforms/streaming-client/run.sh
+++ waveforms/streaming-client/run.sh
@@ -6,5 +6,6 @@ datasource="slink://geofon.gfz-potsdam.de:18000"
 datasource="arclink://webdc.eu:18001"
 datasource="fdsnws://geofon.gfz-potsdam.de/fdsnws/dataselect/1/query"
 datasource="fdsnws://service.iris.edu/fdsnws/dataselect/1/query"
-seiscomp exec python timewindow-client.py --debug -I "$datasource"
+
+seiscomp exec python streaming-client.py --debug -I "$datasource"
 
