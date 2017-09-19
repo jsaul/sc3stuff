a6dd220362615745d785cc446a2ee0e4c8af18fa
Joachim Saul <saul@gfz-potsdam.de>
2017-09-19 14:14:01 +0200

    cleanups

--- playback/notifier/notifier-player.py
+++ playback/notifier/notifier-player.py
@@ -1,29 +1,28 @@
-from __future__ import print_function
+import sys, os
+import seiscomp3.Core, seiscomp3.Client, seiscomp3.DataModel, seiscomp3.IO, seiscomp3.Logging, seiscomp3.Utils
 
-import sys, os, time, tempfile
-from seiscomp3 import Core, Client, DataModel, Communication, IO, Logging, Utils
-
-class NotifierPlayer(Client.Application):
+class NotifierPlayer(seiscomp3.Client.Application):
 
     def __init__(self, argc, argv):
-        Client.Application.__init__(self, argc, argv)
+        super(NotifierPlayer, self).__init__(argc, argv)
         self.setMessagingEnabled(False)
         self.setDatabaseEnabled(False, False)
         self._startTime = self._endTime = None
         self._xmlFileName = None
-        self.speed = 1
+        self._time = None
+#       self.speed = 1
 
     def createCommandLineDescription(self):
-        Client.Application.createCommandLineDescription(self)
+        super(NotifierPlayer, self).createCommandLineDescription()
         self.commandline().addGroup("Play")
         self.commandline().addStringOption("Play", "begin", "specify start of time window")
         self.commandline().addStringOption("Play", "end", "specify end of time window")
-        self.commandline().addStringOption("Play", "speed", "specify speed factor")
+#       self.commandline().addStringOption("Play", "speed", "specify speed factor")
         self.commandline().addGroup("Input")
         self.commandline().addStringOption("Input", "xml-file", "specify xml file")
 
     def init(self):
-        if not Client.Application.init(self):
+        if not super(NotifierPlayer, self).init():
             return False
 
         try:    start = self.commandline().optionString("begin")
@@ -35,77 +34,88 @@ class NotifierPlayer(Client.Application):
         try:    self._xmlFileName = self.commandline().optionString("xml-file")
         except: pass
 
-        try:
-            self.speed = self.commandline().optionString("speed")
-            if self.speed == "0":
-                self.speed = None
-            else:
-                self.speed = float(self.speed)
-        except: self.speed = 1
+#       try:
+#           self.speed = self.commandline().optionString("speed")
+#           if self.speed == "0":
+#               self.speed = None
+#           else:
+#               self.speed = float(self.speed)
+#       except: self.speed = 1
 
         if start:
-            self._startTime = Core.Time.GMT()
-            if self._startTime.fromString(start, "%F %T") == False:
-                Logging.error("Wrong 'begin' format")
+            self._startTime = seiscomp3.Core.Time.GMT()
+            if self._startTime.fromString(start, "%FT%TZ") == False:
+                seiscomp3.Logging.error("Wrong 'begin' format")
                 return False
         if end:
             self._endTime = Core.Time.GMT()
-            if self._endTime.fromString(end, "%F %T") == False:
-                Logging.error("Wrong 'end' format")
+            if self._endTime.fromString(end, "%FT%TZ") == False:
+                seiscomp3.Logging.error("Wrong 'end' format")
                 return False
-
         return True
 
-    def addObject(self, parent, obj):
-        Logging.debug("addObject parent=%s" % parent)
-
-    def updateObject(self, parent, obj):
-        Logging.debug("updateObject parent=%s" % parent)
-
     def _readNotifierMessageFromXML(self, xml):
-        b = Utils.stringToStreambuf(xml)
-        ar = IO.XMLArchive(b)
+        b = seiscomp3.Utils.stringToStreambuf(xml)
+        ar = seiscomp3.IO.XMLArchive(b)
         obj = ar.readObject()
         if obj is None:
             raise TypeError, "got invalid xml"
-        nm  = DataModel.NotifierMessage.Cast(obj)
-        if nm is None:
+        nmsg = seiscomp3.DataModel.NotifierMessage.Cast(obj)
+        if nmsg is None:
             raise TypeError, self._xmlFile + ": no NotifierMessage object found"
-        return nm
-
-    def handleMessage(self, msg):
-        Logging.debug("handleMessage %s" % str(msg))
-        Client.Application.handleMessage(self, msg)
+        return nmsg
 
     def run(self):
         if not self._xmlFileName:
             return False
 
-        Logging.debug("input file is %s" % self._xmlFileName)
+        seiscomp3.Logging.debug("input file is %s" % self._xmlFileName)
         self._xmlFile = file(self._xmlFileName)
 
         while True:
             while True:
-                line = self._xmlFile.readline().strip()
+                line = self._xmlFile.readline()
                 if not line:
+                    # empty input
                     return True
+                line = line.strip()
+                if not line:
+                    # blank line
+                    continue
                 if line[0] == "#":
                     break
-            print (line)
-            try:
-                sharp, nbytes, timestamp = line.split()
-                nbytes = int(nbytes)
-                assert sharp[0] == "#"
-                xml = self._xmlFile.read(nbytes)
-                print(xml)
-            except:
-                Logging.error("ERROR")
+
+            sharp, timestamp, nbytes = line.split()
+            time = seiscomp3.Core.Time.GMT()
+            time.fromString(timestamp, "%FT%T.%fZ")
+
+            if self._startTime is not None and time < self._startTime:
+                continue
+            if self._endTime is not None and time > self._endTime:
                 break
 
-            msg = self._readNotifierMessageFromXML(xml)
-            self.handleMessage(msg)
+            nbytes = int(nbytes)
+            assert sharp[0] == "#"
+            xml = self._xmlFile.read(nbytes)
+
+            nmsg = self._readNotifierMessageFromXML(xml.strip())
+            self.sync(time)
+            self.handleMessage(nmsg)
 
         return True
+
+    def sync(self, time):
+        self._time = time
+        seiscomp3.Logging.debug("sync time=%s" % time.toString("%FT%T.%fZ"))
+
+    def addObject(self, parent, obj):
+        # in a usable player, this must be reimplemented
+        seiscomp3.Logging.debug("addObject class=%s parent=%s" % (obj.className(),parent))
+
+    def updateObject(self, parent, obj):
+        # in a usable player, this must be reimplemented
+        seiscomp3.Logging.debug("updateObject class=%s parent=%s" % (obj.className(),parent))
+
 
 app = NotifierPlayer(len(sys.argv), sys.argv)
 sys.exit(app())
